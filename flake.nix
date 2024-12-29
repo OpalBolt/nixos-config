e7f18e87dc3304f3b000b57762c1d243b2a48caa
Hexamo <mads@plikki.com>
2024-12-29 22:38:33 +0100

    new test

--- flake.nix
+++ flake.nix
@@ -1,47 +1,63 @@
 {
-  description = "Nixos config flake";
+  description = "Nix, Nixos and flake configuraiton";
 
   inputs = {
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # User enviroment manager
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    # Neovim
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+
   };
 
-  outputs =
-    {
-      self,
-      ... 
-    }@inputs:
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    nixvim,
+    home-manager
+  }:
+     let
+       vars = {
+         # User Configuration
+         username = "mads";
+         terminal = "kitty";
+         wallpaper = "default";
+         editor = "nvim";
 
-    let
-      hosts = import ./config/hosts.nix;
+         # System Configuraiton
+         hostname = "ceris";
+         locale = "en_DK.UTF-8";
+         timezone = "Europe/Copenhagen";
+         kbdLayout = "dk-latin1";
 
-      mkNixOSConfigurations = 
-        {
-          host,
-          nixpkgs,
-          home-manager,
-          modules ? []
-        }:
-        nixpkgs.lib.nixosSystem {
-          system = host.arch;
-          modules = [
-            .hosts/${host.dir}/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.users."${host.user}" = import ./hosts/${host.dir}/home.nix;
-            }
-          ] ++ modules;
-        };
-    in
-    {
-      nixosConfigurations."${hosts.work.hostname}" = mkNixOSConfigurations {
-        host = hosts.work;
-        nixpkgs = inputs.nixpkgs;
-        home-manager = inputs.home-manager;
+       };
+       system = "x86_64-linux";
+       lib = nixpkgs.lib;
+     in
+  {
+    nixosConfigurations = {
+      ceris = lib.nixosSystem {
+        inherit system;
+        specialArgs = ( inputs vars );
+        modules = [
+          ./hosts/ceris/configuration.nix
+          nixvim.nixosModules.nixvim
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
       };
-    };
+    };  
+  };
 }
-
