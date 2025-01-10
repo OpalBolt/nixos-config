67cf56bef9ba1d96a16836c754a1f54ae5ff775f
Hexamo <mads@plikki.com>
2025-01-10 16:10:12 +0100

    feat(logic): add structure for apps

---
+++ modules/home-manager/apps/polkit/default.nix
@@ -0,0 +1,14 @@
+{config, lib, ... }:
+
+{
+  options = {
+    module1.enable = 
+      lib.mkEnableOption "Enables module1";
+  };
+
+  config = lib.mkIf config.module1.enable {
+  programs.app = {
+      enable = true;
+    };
+  };
+}
