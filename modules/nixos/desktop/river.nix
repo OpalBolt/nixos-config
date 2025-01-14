39554c0c9aa723e23a9dadd74622dd3f7b44fa8f
Hexamo <mads@plikki.com>
2025-01-14 20:39:34 +0100

    feat(river): Add missing features

--- modules/nixos/desktop/river.nix
+++ modules/nixos/desktop/river.nix
@@ -5,7 +5,34 @@
   ...
 }:
 {
+  options = {
+    feature.desktop.river.enable = lib.mkEnableOption "Enable river and all required apps";
+    default = true;
+  };
+
   config = lib.mkIf config.feature.desktop.river.enable {
+    xdg = {
+      portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-wlr
+          xdg-desktop-portal-gtk
+        ];
+      };
+    };
+
+    environment.sessionVariables = {
+      XDG_CURRENT_DESKTOP = "river";
+    };
+
+    programs = {
+      # enable riverwm
+      river.enable = true;
+
+      # enable waybar
+      waybar.enable = true;
+    };
+
     environment.systemPackages = with pkgs; [
 
       # Screenshot related apps
@@ -39,7 +66,7 @@
       swaybg # Wayland background handler
       wlr-randr # Information about monitor position
       networkmanagerapplet # App for controlling networks
-
+      fuzzel # Replacemnet for Rofi
     ];
   };
 
