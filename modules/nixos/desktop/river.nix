ab597d8e047956c111a171b35ab7340a8920eef1
Hexamo <mads@plikki.com>
2025-02-08 14:30:42 +0100

    fix(firefox): Allow FHS apps to use xgd-open

--- modules/nixos/desktop/river.nix
+++ modules/nixos/desktop/river.nix
@@ -14,6 +14,7 @@
     xdg = {
       portal = {
         enable = true;
+        xdgOpenUsePortal = true;
         extraPortals = with pkgs; [
           xdg-desktop-portal-wlr
           xdg-desktop-portal-gtk
