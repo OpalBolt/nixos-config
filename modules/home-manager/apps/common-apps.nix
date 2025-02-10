2992e5edd2a465f187a5f99e5a492188e72e902a
OpalBolt <git@skumnet.dk>
2025-02-10 21:51:39 +0100

    test(apps): Add startup to nextcloud

--- modules/home-manager/apps/common-apps.nix
+++ modules/home-manager/apps/common-apps.nix
@@ -2,4 +2,9 @@
 
 {
   services.nextcloud-client.enable = true;
+  systemd.user.services.nextcloud-client = {
+    Unit = {
+      After = pkgs.lib.mkForce "graphical-session.target";
+    };
+  };
 }
