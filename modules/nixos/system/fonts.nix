ae7bb0bf37585ba39b2098e8338e8acfc140783d
OpalBolt <git@skumnet.dk>
2025-02-10 21:52:20 +0100

    fix(font): rmeove unused fonts, prioritize nf

--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -1,23 +1,24 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
-    nerdfonts
-    Iosevka
-    fantasque-sans-mono
-    mononoki
-    ubuntu_font_family
-    (nerdfonts.override {
-      fonts = [
-        "Iosevka"
-        "FantasqueSansMono"
-        "Mononoki"
-      ];
-    })
-  ];
-  fontconfig = {
-    defaultFonts = {
-      monospace = [ "Iosevka NFM:style=Regular" ];
+  fonts = {
+    packages = with pkgs; [
+      #nerdfonts
+      #Iosevka
+      fantasque-sans-mono
+      mononoki
+      ubuntu_font_family
+      (nerdfonts.override {
+        fonts = [
+          "Iosevka"
+          "FantasqueSansMono"
+          "Mononoki"
+        ];
+      })
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "Iosevka NFM:style=Regular" ];
+      };
     };
   };
-
 }
