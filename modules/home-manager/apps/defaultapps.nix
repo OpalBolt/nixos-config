909a055232535b44f0ce449dd67561a6eef0ce13
OpalBolt <git@skumnet.dk>
2025-02-12 19:59:02 +0100

    fix(apps): app links not working as intended

--- modules/home-manager/apps/defaultapps.nix
+++ modules/home-manager/apps/defaultapps.nix
@@ -3,24 +3,21 @@
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = "chromium.desktop";
-      "x-scheme-handler/http" = "chromium.desktop";
-      "x-scheme-handler/https" = "chromium.desktop";
-      "x-scheme-handler/about" = "chromium.desktop";
-      "x-scheme-handler/unknown" = "chromium.desktop";
-      # "text/html" = "firefox.desktop";
-      # "x-scheme-handler/http" = "firefox.desktop";
-      # "x-scheme-handler/https" = "firefox.desktop";
-      # "x-scheme-handler/about" = "firefox.desktop";
-      # "x-scheme-handler/unknown" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
     };
   };
   xdg.desktopEntries.firefox = {
     name = "firefox";
-    exec = "${pkgs.firefox}/bin/firefox";
+    exec = "${pkgs.firefox}/bin/firefox %U";
+    type = "Application";
   };
-  xdg.desktopEntries.chromium = {
-    name = "chromium";
-    exec = "${pkgs.ungoogled-chromium}/bin/chromium";
-  };
+  # xdg.desktopEntries.chromium = {
+  #   name = "chromium";
+  #   exec = "${pkgs.ungoogled-chromium}/bin/chromium %U";
+  #   type = "Application";
+  # };
 }
