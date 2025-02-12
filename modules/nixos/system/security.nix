64d068021797641d7337ba38670d4242c7080caa
OpalBolt <git@skumnet.dk>
2025-02-12 21:40:57 +0100

    feat(security): enable security features
    * Confiure that only @wheel users are allowed to run sudo
    * ensure that only @whel cusers can run nixpkgs
    * Remote default packages
    * Disable coredump
    * Run chrome application in sandbox
    * Enable ClamAV
    * Disable SSHD Services

---
+++ modules/nixos/system/security.nix
@@ -0,0 +1,75 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  options = {
+    feature.security = {
+      enable = lib.mkEnableOption "General security" // {
+        default = true;
+      };
+      av.enable = lib.mkEnableOption "Enables ClamAV" // {
+        default = true;
+      };
+      audit.enable = lib.mkEnableOption "Enables audit" // {
+        default = true;
+      };
+      sshd-disable = lib.mkEnableOption "Disables SSHD features" // {
+        default = true;
+      };
+
+    };
+  };
+
+  config = lib.mkIf config.feature.security.enable (
+    lib.mkMerge [
+
+      # only allow users that has sudo permissions to interact with nix pkgs
+      {
+        nix.allowedUsers = [ "@wheel" ];
+        security.sudo.execWheelOnly = true;
+        environment.defaultPackages = lib.mkForce [ ];
+        systemd.coredump.enable = false;
+        security.chromiumSuidSandbox.enable = true;
+
+      }
+
+      (lib.mkIf config.feature.security.av.enable {
+        # Install requried software
+        environment.systemPackages = [
+          pkgs.clamav
+        ];
+
+        # Enable ClamAV
+        services.clamav.daemon.enable = config.feature.security.av.enable;
+        services.clamav.updater.enable = config.feature.security.av.enable;
+      })
+      (lib.mkIf config.feature.security.audit.enable {
+        security.auditd.enable = true;
+        security.audit.enable = true;
+        security.audit.rules = [
+          "-a exit,always -F arch=b64 -S execve"
+        ];
+
+      })
+      (lib.mkIf config.feature.security.sshd-disable {
+        services.openssh = {
+          passwordAuthentication = false;
+          allowSFTP = false; # Don't set this if you need sftp
+          challengeResponseAuthentication = false;
+          extraConfig = ''
+            AllowTcpForwarding yes
+            X11Forwarding no
+            AllowAgentForwarding no
+            AllowStreamLocalForwarding no
+            AuthenticationMethods publickey
+          '';
+        };
+
+      })
+    ]
+  );
+}
