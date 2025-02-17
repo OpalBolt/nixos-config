d5bbe14710867b417deae2498c0b9fa3f1ff3c7a
OpalBolt <git@skumnet.dk>
2025-02-17 06:51:59 +0100

    fix(virt): enable rooted docker

--- modules/nixos/system/virtualization.nix
+++ modules/nixos/system/virtualization.nix
@@ -12,10 +12,7 @@
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
-    docker.rootless = {
-      enable = true;
-      setSocketVariable = true;
-    };
+    docker.enable = true;
 
   };
 
