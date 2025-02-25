48c94594cd4f38cfbf824dd59a14943f2ddfd237
OpalBolt <git@skumnet.dk>
2025-02-25 06:20:33 +0100

    fix(sec): Simplify secutiry management

--- modules/nixos/system/security.nix
+++ modules/nixos/system/security.nix
@@ -6,74 +6,46 @@
 }:
 
 {
-  options = {
-    feature.security = {
-      enable = lib.mkEnableOption "General security" // {
-        default = true;
-      };
-      av.enable = lib.mkEnableOption "Enables ClamAV" // {
-        default = true;
-      };
-      audit.enable = lib.mkEnableOption "Enables audit" // {
-        default = true;
-      };
-      sshd-disable = lib.mkEnableOption "Disables SSHD features" // {
-        default = true;
-      };
 
+  # only allow users that has sudo permissions to interact with nix pkgs
+  nix.settings.allowed-users = [ "@wheel" ];
+  security.sudo.execWheelOnly = true;
+  environment.defaultPackages = lib.mkForce [ ];
+  systemd.coredump.enable = false;
+  security.chromiumSuidSandbox.enable = true;
+
+  # Install requried software
+  environment.systemPackages = [
+    pkgs.clamav
+  ];
+
+  # Enable ClamAV
+  services.clamav = {
+    daemon.enable = true;
+    updater.enable = true;
+  };
+  security.auditd.enable = true;
+  security.audit.enable = true;
+  security.audit.rules = [
+    "-a exit,always -F arch=b64 -S execve"
+  ];
+
+  services.openssh = {
+    settings = {
+      passwordAuthentication = false;
+      challengeResponseAuthentication = false;
     };
+    allowSFTP = false; # Don't set this if you need sftp
+    extraConfig = ''
+      AllowTcpForwarding yes
+      X11Forwarding no
+      AllowAgentForwarding no
+      AllowStreamLocalForwarding no
+      AuthenticationMethods publickey
+    '';
   };
 
-  config = lib.mkIf config.feature.security.enable (
-    lib.mkMerge [
-
-      # only allow users that has sudo permissions to interact with nix pkgs
-      {
-        nix.settings.allowed-users = [ "@wheel" ];
-        security.sudo.execWheelOnly = true;
-        environment.defaultPackages = lib.mkForce [ ];
-        systemd.coredump.enable = false;
-        security.chromiumSuidSandbox.enable = true;
-
-      }
-
-      (lib.mkIf config.feature.security.av.enable {
-        # Install requried software
-        environment.systemPackages = [
-          pkgs.clamav
-        ];
-
-        # Enable ClamAV
-        services.clamav = {
-          daemon.enable = true;
-          updater.enable = true;
-        };
-      })
-      (lib.mkIf config.feature.security.audit.enable {
-        security.auditd.enable = true;
-        security.audit.enable = true;
-        security.audit.rules = [
-          "-a exit,always -F arch=b64 -S execve"
-        ];
-
-      })
-      (lib.mkIf config.feature.security.sshd-disable {
-        services.openssh = {
-          settings = {
-            passwordAuthentication = false;
-            challengeResponseAuthentication = false;
-          };
-          allowSFTP = false; # Don't set this if you need sftp
-          extraConfig = ''
-            AllowTcpForwarding yes
-            X11Forwarding no
-            AllowAgentForwarding no
-            AllowStreamLocalForwarding no
-            AuthenticationMethods publickey
-          '';
-        };
-
-      })
-    ]
-  );
+  security.loginDefs.settings = {
+    PASS_MIN_DAYS = 1;
+  };
 }
