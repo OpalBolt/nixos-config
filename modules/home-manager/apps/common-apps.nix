c17144c61763f0340879703d4254bccdba477415
OpalBolt <git@skumnet.dk>
2025-02-25 18:58:23 +0100

    feat(apps): remove nextcloud service

--- modules/home-manager/apps/common-apps.nix
+++ modules/home-manager/apps/common-apps.nix
@@ -2,9 +2,4 @@
 
 {
   services.nextcloud-client.enable = true;
-  systemd.user.services.nextcloud-client = {
-    Unit = {
-      After = pkgs.lib.mkForce "graphical-session.target";
-    };
-  };
 }
