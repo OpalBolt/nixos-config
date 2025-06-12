21dae8b828b1666aae81efcb033a3ffc2d637f32
OpalBolt <git@skumnet.dk>
2025-06-12 21:22:35 +0200

    feat: Remove dependencies on features

--- modules/home-manager/shell/zsh.nix
+++ modules/home-manager/shell/zsh.nix
@@ -1,120 +1,119 @@
 { config, lib, ... }:
 
 {
-  options = {
-    feature.shell.zsh.enable = lib.mkEnableOption "Enables zsh shell" // {
-      default = true;
+  programs.zoxide.enable = true;
+  programs.zoxide.enableZshIntegration = true;
+
+  programs.eza.enable = true;
+  programs.eza.enableZshIntegration = true;
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+    history = {
+      ignoreSpace = true;
+      ignoreDups = true;
+      size = 1000000;
     };
-  };
-
-  config = lib.mkIf config.feature.shell.zsh.enable {
-    programs.zoxide.enable = true;
-    programs.zoxide.enableZshIntegration = true;
-
-    programs.eza.enable = true;
-    programs.eza.enableZshIntegration = true;
-
-    programs.zsh = {
+    oh-my-zsh = {
       enable = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-      history = {
-        ignoreSpace = true;
-        ignoreDups = true;
-        size = 1000000;
-      };
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "colored-man-pages"
-          "gitignore"
-          #"command-not-found"
-          "fzf"
-          "ssh-agent"
-          "git"
-          "git-extras"
-          "aws"
-          "docker"
-          "docker-compose"
-          "kubectl"
-          "opentofu"
-          "kind"
-          "kitty"
-        ];
-      };
-      zplug = {
-        enable = true;
-        plugins = [
-          {
-            name = "djui/alias-tips";
-            tags = [
-              "from:github"
-              "as:plugin"
-            ];
-          }
-          {
-            name = "zsh-users/zsh-autosuggestions";
-            tags = [
-              "from:github"
-              "as:plugin"
-            ];
-          }
-          {
-            name = "zsh-users/zsh-syntax-highlighting";
-            tags = [
-              "from:github"
-              "as:plugin"
-              "defer:2"
-            ];
-          }
-          {
-            name = "svenXY/timewarrior";
-            tags = [
-              "from:github"
-              "as:plugin"
-            ];
-          }
-        ];
-      };
+      plugins = [
+        "colored-man-pages"
+        "gitignore"
+        #"command-not-found"
+        "fzf"
+        "ssh-agent"
+        "git"
+        "git-extras"
+        "aws"
+        "docker"
+        "docker-compose"
+        "kubectl"
+        "opentofu"
+        "kind"
+        "kitty"
+      ];
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        {
+          name = "djui/alias-tips";
+          tags = [
+            "from:github"
+            "as:plugin"
+          ];
+        }
+        {
+          name = "zsh-users/zsh-autosuggestions";
+          tags = [
+            "from:github"
+            "as:plugin"
+          ];
+        }
+        {
+          name = "zsh-users/zsh-syntax-highlighting";
+          tags = [
+            "from:github"
+            "as:plugin"
+            "defer:2"
+          ];
+        }
+        {
+          name = "svenXY/timewarrior";
+          tags = [
+            "from:github"
+            "as:plugin"
+          ];
+        }
+      ];
+    };
 
-      shellAliases = {
+    shellAliases = {
 
-        wb = "systemctl --user restart waybar.service && nextcloud --background & disown nextcloud";
+      wb = "systemctl --user restart waybar.service && nextcloud --background & disown nextcloud";
 
-        # Set screens
-        wlh = "bash ~/scripts/wlr/home.sh";
-        wlw = "bash ~/scripts/wlr/work.sh";
+      # Set screens
+      wlh = "bash ~/scripts/wlr/home.sh";
+      wlw = "bash ~/scripts/wlr/work.sh";
 
-        # Ensure -al is always used
-        ls = "eza -mh1la --classify=always --icons=always --color=always --group-directories-first --git";
+      # Ensure -al is always used
+      ls = "eza -mh1la --classify=always --icons=always --color=always --group-directories-first --git";
 
-        # Dont use vim by mistake
-        vim = "nvim";
-        vi = "nvim";
+      # Dont use vim by mistake
+      vim = "nvim";
+      vi = "nvim";
 
-        # use g instead of git
-        g = "git";
-        lg = "lazygit";
+      # use g instead of git
+      g = "git";
+      lg = "lazygit";
 
-        # use zoxide over cd
-        cd = "z";
+      # use zoxide over cd
+      cd = "z";
 
-        # add a newline before neofetch
-        neofetch = "fastfetch";
+      # add a newline before neofetch
+      neofetch = "fastfetch";
 
-        # runs the customer.sh script.
-        cu = "bash ~/scripts/customers.sh";
+      # runs the customer.sh script.
+      cu = "bash ~/scripts/customers.sh";
 
-        # uploads files to gdrive and add it to git
-        cup = "bash ~/scripts/uploadcustdocument.sh";
+      # uploads files to gdrive and add it to git
+      cup = "bash ~/scripts/uploadcustdocument.sh";
 
-        # Run git-leaks on current directory
-        gle = "podman run --rm -v $\{\PWD}\:/path docker.io/zricethezav/gitleaks:latest git --verbose /path";
+      # Run git-leaks on current directory
+      gle = "podman run --rm -v $\{\PWD}\:/path docker.io/zricethezav/gitleaks:latest git --verbose /path";
 
-        # Better way of getting IP information
-        ipa = "ip -br -c a && echo --- && ip -br -c l";
-      };
+      # Better way of getting IP information
+      ipa = "ip -br -c a && echo --- && ip -br -c l";
+
+      # =
+      # ========== Work related ==========
+      # =
+
+      ovpnw = "openvpn --config /run/secrets/openvpn-work";
+
     };
   };
 }
