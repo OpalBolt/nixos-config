9839f876a595f2c6bb740b9c254254fe058a513a
OpalBolt <git@skumnet.dk>
2025-06-24 20:22:32 +0200

    feat: remove unused fonts, add support for complex fonts

--- home/global/core/fonts.nix
+++ home/global/core/fonts.nix
@@ -1,15 +1,15 @@
 { pkgs, lib, ... }:
 {
   fonts.fontconfig.enable = true;
-  
+
   home.packages = with pkgs; [
     # Core font selection - minimal but functional
     noto-fonts
     noto-fonts-emoji
-    
+
     # Programming fonts
-    (nerdfonts.override { fonts = [ "Iosevka" "JetBrainsMono" ]; })
-    
+    nerd-fonts.iosevka
+
     # UI fonts
     lexend # Highly readable font for interfaces
   ];
