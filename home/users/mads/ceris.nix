5ee4e1acd1b2e2c2c2999d21c09ee62187f39599
OpalBolt <git@skumnet.dk>
2025-06-24 20:48:19 +0200

    fix: Add flatten to imports

--- home/users/mads/ceris.nix
+++ home/users/mads/ceris.nix
@@ -4,7 +4,12 @@
   ...
 }:
 {
-  imports = [
+  imports = lib.flatten [
+        (map lib.custom.relativeToRoot [
+          "home/global/core"
+          "home/global/common/sys/complex-fonts.nix"
+          ])
+
     (lib.custom.relativeToRoot "home/global/core")
     ./core
   ];
