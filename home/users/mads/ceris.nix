3090a74b4d8e7e12d27b62ef25e965206617eb82
OpalBolt <git@skumnet.dk>
2025-06-27 23:38:51 +0200

    feat: import configs

--- home/users/mads/ceris.nix
+++ home/users/mads/ceris.nix
@@ -7,8 +7,14 @@
   imports = lib.flatten [
     (map lib.custom.relativeToRoot [
       "home/global/core"
+      "home/global/common/browsers/firefox.nix"
+      "home/global/common/comms/discord.nix"
+      "home/global/common/shell/kitty.nix"
       "home/global/common/sys/complex-fonts.nix"
       "home/global/common/tools/bat.nix"
+      "home/global/common/tools/direnv.nix"
+
+      #"home/global/common/"
     ])
 
     ./core
