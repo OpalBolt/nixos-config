af556291b95816570c4dbc09584726dbf3ee899b
OpalBolt <git@skumnet.dk>
2025-06-27 23:44:44 +0200

    fix(hm): update system var to be pulled from hostSpec

--- home/global/common/browsers/firefox.nix
+++ home/global/common/browsers/firefox.nix
@@ -45,7 +45,7 @@
         name = hostSpec.username;
         isDefault = true;
 
-        extensions.packages = with inputs.nur.legacyPackages."${vars.system}".repos.rycee.firefox-addons; [
+        extensions.packages = with inputs.nur.legacyPackages."${hostSpec.system}".repos.rycee.firefox-addons; [
           ublock-origin
           display-_anchors
           reddit-enhancement-suite
