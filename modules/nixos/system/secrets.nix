eaa63e9fbb35a9c1acd61027466676456d4f9144
OpalBolt <git@skumnet.dk>
2025-06-27 18:14:39 +0200

    fix: paths for secrets

--- modules/nixos/system/secrets.nix
+++ modules/nixos/system/secrets.nix
@@ -14,37 +14,37 @@ in
   };
 
   sops.secrets.openvpn-work = {
-    sopsFile = "${secretspath}/secrets/ceris.yaml";
-    key = "openvpn/work";
+    sopsFile = "${secretspath}/secrets/ceris-vpn.yaml";
+    key = "openvpn";
   };
 
-  sops.secrets."mads/ssh-key-pub" = {
+  sops.secrets."opalbolt/ssh-key-pub" = {
     owner = config.users.users.mads.name;
     path = "/home/mads/.ssh/id_ed25519.pub";
   };
-  sops.secrets."mads/ssh-key-priv" = {
+  sops.secrets."opalbolt/ssh-key-priv" = {
     owner = config.users.users.mads.name;
     path = "/home/mads/.ssh/id_ed25519";
   };
 
   sops.secrets.ca_cert = {
     owner = config.users.users.mads.name;
-    sopsFile = "${secretspath}/secrets/workwifi.yaml";
+    sopsFile = "${secretspath}/secrets/ceris-wifi.yaml";
   };
 
   sops.secrets.user_cert = {
     owner = config.users.users.mads.name;
-    sopsFile = "${secretspath}/secrets/workwifi.yaml";
+    sopsFile = "${secretspath}/secrets/ceris-wifi.yaml";
   };
 
   sops.secrets.user_key = {
     owner = config.users.users.mads.name;
-    sopsFile = "${secretspath}/secrets/workwifi.yaml";
+    sopsFile = "${secretspath}/secrets/ceris-wifi.yaml";
   };
 
   sops.secrets.key_password = {
     owner = config.users.users.mads.name;
-    sopsFile = "${secretspath}/secrets/workwifi.yaml";
+    sopsFile = "${secretspath}/secrets/ceris-wifi.yaml";
   };
 
 }
