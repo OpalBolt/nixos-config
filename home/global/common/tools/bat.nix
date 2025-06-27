20b9f0350c1a9f8723875b21e89c8ff8fb9b9fe4
OpalBolt <git@skumnet.dk>
2025-06-27 23:37:45 +0200

    fix(hm): bat theme config

--- home/global/common/tools/bat.nix
+++ home/global/common/tools/bat.nix
@@ -8,18 +8,21 @@
     config = {
       # Git modifications and file header (but no grid)
       style = "changes,header";
-      theme = {
-        "kanagawa" = {
-          src = pkgs.fetchFromGitHub {
-            owner = "rebelot";
-            repo = "kanagawa.nvim"; # Bat uses sublime syntax for its themes
-            rev = "debe91547d7fb1eef34ce26a5106f277fbfdd109";
-            sha256 = "sha256-i54hTf4AEFTiJb+j5llC5+Xvepj43DiNJSq0vPZCIAg=";
-          };
-          file = "extras/tmTheme/kanagawa.tmTheme";
+      theme = "kanagawa";
+    };
+
+    themes = {
+      "kanagawa" = {
+        src = pkgs.fetchFromGitHub {
+          owner = "rebelot";
+          repo = "kanagawa.nvim"; # Bat uses sublime syntax for its themes
+          rev = "debe91547d7fb1eef34ce26a5106f277fbfdd109";
+          sha256 = "sha256-i54hTf4AEFTiJb+j5llC5+Xvepj43DiNJSq0vPZCIAg=";
         };
+        file = "extras/tmTheme/kanagawa.tmTheme";
       };
     };
+
     extraPackages = builtins.attrValues {
       inherit (pkgs.bat-extras)
 
