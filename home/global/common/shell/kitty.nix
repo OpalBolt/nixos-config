b4e2689beb91c8ea917e789e506d5955496f2d1e
OpalBolt <git@skumnet.dk>
2025-06-27 23:37:33 +0200

    feat(hm): add kitty

---
+++ home/global/common/shell/kitty.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  ...
+}:
+
+{
+  programs.kitty = {
+    enable = true;
+    themeFile = "kanagawa";
+    font = {
+      name = "IosevkaTerm Nerd Font Mono";
+      size = 12;
+    };
+    keybindings = {
+      "alt+shift++" = "send_key alt+?";
+    };
+    shellIntegration = {
+      enableZshIntegration = true;
+      mode = "enabled";
+    };
+    settings = {
+      background_opacity = 0.85;
+      cursor_trail = 0;
+      hide_window_decorations = true;
+      window_border_width = 0;
+      draw_minimal_borders = true;
+      shell_integration = false;
+      wheel_scroll_multiplier = 5.0;
+      input_delay = 3;
+      allow_remote_control = true;
+      listen_on = "unix:@mykitty";
+    };
+  };
+}
