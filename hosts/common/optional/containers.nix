54beea6b59e7fed71b49af471adbe301662f3666
OpalBolt <git@skumnet.dk>
2025-07-01 20:02:53 +0200

    feat: add container support

---
+++ hosts/common/optional/containers.nix
@@ -0,0 +1,22 @@
+{ pkgs, vars, ... }:
+
+{
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = false;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    docker.enable = true;
+
+  };
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+  ];
+}
