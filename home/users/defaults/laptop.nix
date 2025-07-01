7dc79d4c098546597fe38a2ee94bc319cf465213
OpalBolt <git@skumnet.dk>
2025-07-01 18:39:25 +0200

    feat: Add default pkgs for laptops

---
+++ home/users/defaults/laptop.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  hostSpec,
+  ...
+}:
+{
+  imports = lib.flatten [
+    (map lib.custom.relativeToRoot [
+      # Browsers
+      "home/global/common/browsers/firefox.nix"
+
+      # Desktop Environment
+      "home/global/common/desktop/gtk.nix"
+      "home/global/common/desktop/river.nix"
+
+      # Editors
+      "home/global/common/editors/neovim.nix"
+
+      # Office
+      "home/global/common/productivity/libreoffice.nix"
+      "home/global/common/productivity/nextcloud.nix"
+      "home/global/common/productivity/thunderbird.nix"
+
+      # Shell Apps
+      "home/global/common/shell/common-tools.nix"
+      "home/global/common/shell/kitty.nix"
+      "home/global/common/shell/starship.nix"
+      "home/global/common/shell/tealdeer.nix"
+      "home/global/common/shell/yazi.nix"
+      "home/global/common/shell/zellij.nix"
+      "home/global/common/shell/zsh.nix"
+
+      # System and General Purpose Tools
+      "home/global/common/sys/complex-fonts.nix"
+      "home/global/common/sys/xdg.nix"
+
+      # Tools
+      "home/global/common/tools/bitwarden.nix"
+      "home/global/common/tools/nix-related.nix"
+    ])
+
+    ./core
+  ];
+
+}
