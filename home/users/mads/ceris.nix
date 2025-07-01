ab59c1be5e728331084d3a14930d17835c8d8a39
OpalBolt <git@skumnet.dk>
2025-07-01 18:39:35 +0200

    feat: Add laptop defaults to Ceris

--- home/users/mads/ceris.nix
+++ home/users/mads/ceris.nix
@@ -7,13 +7,7 @@
   imports = lib.flatten [
     (map lib.custom.relativeToRoot [
       "home/global/core"
-      "home/global/common/browsers/firefox.nix"
-      "home/global/common/comms/discord.nix"
-      "home/global/common/shell/kitty.nix"
-      "home/global/common/sys/complex-fonts.nix"
-      "home/global/common/tools/bat.nix"
-      "home/global/common/tools/direnv.nix"
-      "home/global/common/desktop/river.nix"
+      "home/global/users/defaults/laptop.nix"
       
 
       #"home/global/common/"
