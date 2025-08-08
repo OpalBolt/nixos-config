2b19cbdbcd3789480630303d9c6dc19d69b222ba
Opalbolt <git@skumnet.dk>
2025-08-08 07:00:17 +0200

    feat: Add vscode settings

--- home/global/common/editors/vscode.nix
+++ home/global/common/editors/vscode.nix
@@ -18,12 +18,12 @@
         "[yaml]" = {
           "editor.defaultFormatter" = "redhat.vscode-yaml";
         };
-        "github.copilot.enable": {
-          "*": true,
-          "plaintext": false,
-          "markdown": true,
-          "scminput": false
-        }
+        "github.copilot.enable" = {
+          "*" = true;
+          "plaintext" = false;
+          "markdown" = true;
+          "scminput" = false;
+        };
       };
       extensions = with pkgs; [
 
