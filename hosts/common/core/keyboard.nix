b6a310e3cae3ab4ca610cc0828a1ba45c1eee7ae
Opalbolt <git@skumnet.dk>
2025-09-01 20:37:34 +0200

    feat: single place to set keyboard settings

--- hosts/common/core/keyboard.nix
+++ hosts/common/core/keyboard.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  ...
+}:
 {
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  services.xserver = {
+    xkb.layout = config.hostSpec.kbdLayout;
+    #variant = "";
   };
 
   # use xkb options in tty
