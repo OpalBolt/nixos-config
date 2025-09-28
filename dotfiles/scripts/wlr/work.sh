8a94db7fa2ea95e90dd3d1794e5b914a24393cba
Opalbolt <git@skumnet.dk>
2025-09-28 11:24:19 +0200

    chore(scripts): New screen position and scaling

--- dotfiles/scripts/wlr/work.sh
+++ dotfiles/scripts/wlr/work.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wlr-randr --output eDP-1 --scale 2
+wlr-randr --output eDP-1 --scale 1.7
 wlr-randr --output eDP-1 --pos 0,1440
 wlr-randr --output DP-9 --pos 956,0
 wlr-randr --output DP-11 --pos 3516,0
