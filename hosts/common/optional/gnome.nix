5cf503320ad848fbea4eb10fb2c8c6e0e6930689
Opalbolt <git@skumnet.dk>
2025-10-02 10:49:20 +0200

    feat(deskop): Add support for Gnome

---
+++ hosts/common/optional/gnome.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+
+  services.displayManager.ly = { enable = true; };
+
+  xdg = {
+    portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+
+      # Extra portals handled by the xdg-portal.wlr.enable
+      extraPortals = with pkgs;
+        [
+          xdg-desktop-portal-gnome
+          #xdg-desktop-portal-gtk
+        ];
+    };
+  };
+  services.xserver = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+  environment.gnome.excludePackages = (with pkgs; [
+    atomix # puzzle game
+    epiphany # web browser
+    evince # document viewer
+    geary # email reader
+    gnome-music
+    gnome-photos
+    gnome-terminal
+    gnome-tour
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+  ]);
+  environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+
+}
