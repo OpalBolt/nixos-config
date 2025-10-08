ab3f851b84a00c22b38d510f8a11a1c2ad0b2b32
Opalbolt <git@skumnet.dk>
2025-10-08 13:52:49 +0200

    test(hibernation): Adding hibernation for rosi

--- hosts/common/optional/laptop.nix
+++ hosts/common/optional/laptop.nix
@@ -1,5 +1,27 @@
 { pkgs, ... }:
 {
   services.power-profiles-daemon.enable = true;
-  services.logind.lidSwitch = "hibernate";
+  services.logind.lidSwitch = "suspend-then-hibernate";
+  #boot.kernelParams = [ "resume_offset=<offset>" ];
+  boot.kernelParams = [
+    #"mem_sleep_default=deep"
+    "resume_offset=69933056"
+  ];
+
+  boot.resumeDevice = "/dev/disk/by-uuid/1dfb6a12-e0b1-4910-8e61-6cb43d9838f7";
+  services.logind.powerKey = "hibernate";
+  services.logind.powerKeyLongPress = "poweroff";
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=30m
+    SuspendState=mem
+  '';
+
+  powerManagement.enable = true;
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 32 * 1024; # 32GB in MB
+    }
+  ];
 }
