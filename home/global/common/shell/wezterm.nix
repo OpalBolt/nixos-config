0f14bea7cee4c43d8f4c13cbb6686a645cae7b5f
Opalbolt <git@skumnet.dk>
2025-10-13 21:51:17 +0200

    fix(term): Missing wezterm integrations

--- home/global/common/shell/wezterm.nix
+++ home/global/common/shell/wezterm.nix
@@ -6,7 +6,7 @@
 {
   programs.wezterm = {
     enable = true;
-    extraConfig = (builtins.readFile (lib.custom.relativeToRoot "dotfiles/wezterm/config.lua"));
+    #extraConfig = (builtins.readFile (lib.custom.relativeToRoot "dotfiles/wezterm/config.lua"));
     colorSchemes = {
       kanagawa = {
         ansi = [
