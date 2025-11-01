a8adad88d2c590eb618a1fdf9c9f8fd8017bbf77
Opalbolt <git@skumnet.dk>
2025-11-01 20:09:28 +0100

    feat: Add work wireguard vpn

--- hosts/common/optional/work-vpn.nix
+++ hosts/common/optional/work-vpn.nix
@@ -15,4 +15,32 @@ in
     sopsFile = "${secretspath}/secrets/${config.hostSpec.hostname}-vpn.yaml";
     key = "openvpn";
   };
+  sops.secrets = {
+    wireguard-privatekey = {
+      sopsFile = "${secretspath}/secrets/${config.hostSpec.hostname}-vpn.yaml";
+      key = "wireguard/PrivateKey";
+    };
+  };
+  networking.wg-quick.interfaces = {
+    wireguard-work = {
+      autostart = true;
+      address = [
+        "192.168.8.8/32"
+      ];
+      dns = [
+        "192.168.8.1"
+      ];
+      privateKeyFile = config.sops.secrets.wireguard-privatekey.path;
+
+      peers = [
+        {
+          publicKey = inputs.nix-secrets.Wireguard-work.server-public-key;
+          allowedIPs = inputs.nix-secrets.Wireguard-work.allowedips;
+
+          #allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = inputs.nix-secrets.Wireguard-work.endpoint;
+        }
+      ];
+    };
+  };
 }
