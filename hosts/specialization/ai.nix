bf9a1ad118952b0b80ebba97d086454027253794
Opalbolt <git@skumnet.dk>
2025-12-17 09:02:59 +0100

    feat: add AI cli tools

--- hosts/specialization/ai.nix
+++ hosts/specialization/ai.nix
@@ -1,15 +1,23 @@
 {
+  config,
+  lib,
   pkgs,
   ...
 }:
+let
+  username = config.hostSpec.username;
+in
 {
   specialisation.ai = {
     inheritParentConfig = true;
 
     configuration = {
-      home-manager.users.mads = {
-        imports = [
-          ../../home/global/common/ai/claude-cli.nix
+      home-manager.users.${username} = {
+        imports = lib.flatten [
+          (map lib.custom.relativeToRoot [
+            #"home/global/common/ai/claude-cli.nix"
+            #"home/global/common/ai/gemini-cli.nix"
+          ])
         ];
       };
     };
