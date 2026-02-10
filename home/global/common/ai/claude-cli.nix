f45a06a6ab2f3b76f3ee957ca87202eb6f96eb07
Opalbolt <git@skumnet.dk>
2026-02-10 12:46:26 +0100

    feat(ai): Add zai suppport

--- home/global/common/ai/claude-cli.nix
+++ home/global/common/ai/claude-cli.nix
@@ -1,9 +1,37 @@
-{ pkgs, ... }:
-
 {
+  inputs,
+  config,
+  hostSpec,
+  ...
+}:
+let
+  customersPath =
+    config.home.sessionVariables.CUSTOMERS_PATH or "${config.home.homeDirectory}/git/work/customers";
+  secretspath = builtins.toString inputs.nix-secrets.outPath;
+in
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  sops.secrets = {
+    zai.claudetoken = {
+      sopsFile = "${secretspath}/secrets/per-mads.yaml";
+    };
+
   programs.claude-code = {
     enable = true;
     package = pkgs.unstable.claude-code;
   };
+  home.sessionVariables = {
+    ANTHROPIC_BASE_URL = "https://api.z.ai/api/anthropic";
+    API_TIMEOUT_MS = "3000000";
+  };
+  
+  programs.zsh.initExtra = ''
+     if [ -f "${config.sops.secrets.zai.claudetoken}" ]; then
+       export ANTHROPIC_AUTH_TOKEN=$(cat ${config.sops.secrets.zai.claudetoken})
+     fi
+   '';
 
 }
