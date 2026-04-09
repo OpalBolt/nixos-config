039d71b775047ec6b56781124892c233b16eb595
Opalbolt <git@skumnet.dk>
2026-04-09 08:10:36 +0200

    fix: Switch to stable branc after security incident

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Core nixpkgs channels
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # User environment management
