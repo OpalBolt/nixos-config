a7e3681bab6ead5652058257ab15be262d1f0d05
OpalBolt <git@skumnet.dk>
2026-04-23 09:07:02 +0200

    fix: use unsbale zellij.

--- home/global/common/shell/zellij.nix
+++ home/global/common/shell/zellij.nix
@@ -1,11 +1,13 @@
 {
   lib,
+  pkgs,
   ...
 }:
 
 {
   xdg.configFile."zellij/config.kdl".source = lib.custom.relativeToRoot "dotfiles/zellij/config.kdl";
   programs.zellij = {
+    package = pkgs.unstable.zellij;
     enable = true;
     enableZshIntegration = true;
     enableFishIntegration = true;
